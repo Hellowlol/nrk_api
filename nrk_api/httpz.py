ae9674976e44bfd1acea7f1b0e7b9fa03ac78c85
Hellowlol <hellowlol1@gmail.com>
2017-07-30 23:37:17 +0200

    add exception logging to to http.

--- nrk_api/httpz.py
+++ nrk_api/httpz.py
@@ -1,3 +1,4 @@
+import logging
 from asyncio import ensure_future
 
 import aiohttp
@@ -9,6 +10,8 @@ try:
 except ImportError:
     import json
 
+LOG = logging.getLogger(__name__)
+
 API_URL = 'https://tvapi.nrk.no/v1/'
 HEADERS = {'app-version-android': '999',
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36'}
@@ -16,30 +19,27 @@ HEADERS = {'app-version-android': '999',
 APICALLS = 0
 
 
-async def fetch(sess, url, type='json'):
+async def fetch(sess, url, rtype='json'):
     async with sess.get(url, headers=HEADERS) as response:
-        #print(response.url)
-        #global APICALLS
-        #APICALLS += 1
-        #print(APICALLS) # remove this later..
-
         try:
-            if type == 'json':
+            if rtype == 'json':
                 return await response.json(loads=json.loads)
             else:
                 return await response.text()
-        except:
+        except Exception as e:
+            LOG.exception(e)
+
             return {}
 
 
-async def httpclient(url, conn=None, session=None, type='json'):
+async def httpclient(url, conn=None, session=None, rtype='json'):
     # Roll your own client if you need to set any limits or disable
     # verify certs
-    if type == 'json':
+    if rtype == 'json':
         url = API_URL + url
 
     if session is None:
         session = aiohttp.ClientSession(connector=conn, json_serialize=json.dumps)
 
     async with session as sess:
-        return await ensure_future(fetch(sess, url, type=type))
+        return await ensure_future(fetch(sess, url, rtype=rtype))
